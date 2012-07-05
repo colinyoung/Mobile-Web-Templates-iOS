8aae68857aa80efbeb53e92118151d55f0986480
Colin Young <me@colinyoung.com>
2012-07-05 11:04:48 -0500

    load HTML with a test.

--- NewFramework/Javascripts/boot.js
+++ NewFramework/Javascripts/boot.js
@@ -1,4 +1,7 @@
 /* Let's test javascript communicating back to the objective-c. */
 $(function () {
+  
+  $('body').append('<a href="mwt:loadHTML">Load HTML</a>');
+  
   var $f = new MobileFramework($('body'));
 });
