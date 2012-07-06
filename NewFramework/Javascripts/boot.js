a0c30afbd8be1dbd2baac961500c54308a5ee06d
Colin Young <me@colinyoung.com>
2012-07-06 00:45:08 -0500

    Call document ready manually when boot is ready.

--- NewFramework/Javascripts/boot.js
+++ NewFramework/Javascripts/boot.js
@@ -1,7 +1,9 @@
 /* Let's test javascript communicating back to the objective-c. */
-$(function () {
-  
-  $('body').append('<a href="mwt:loadHTML">Load HTML</a>');
-  
-  var $f = new MobileFramework($('body'));
+var $f = null;
+
+$(function () {  
+  /* Intentionally global.
+     I'm sure there's some better way to put vars in global scope. */
+  $f = new MobileFramework($('body'));
+  $f.callback("mwt:documentReady");
 });
