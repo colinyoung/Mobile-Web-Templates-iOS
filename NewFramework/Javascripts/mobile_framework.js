ac29913a408df3339dd9a78593aee5860b093261
Colin Young <me@colinyoung.com>
2012-07-06 00:45:51 -0500

    Start extracting/repeating templates.

--- NewFramework/Javascripts/mobile_framework.js
+++ NewFramework/Javascripts/mobile_framework.js
@@ -24,6 +24,20 @@ MobileFramework.prototype = {
     document.documentElement.appendChild(iframe);
     iframe.parentNode.removeChild(iframe);
     iframe = null;
+  },
+  extractTemplatingElements: function(html) {
+    var rootElement = $(html);
+    var elements = [];
+    if (rootElement.hasClass(this.prefix)) {
+      elements.push(rootElement);
+    }
+    elements.concat(rootElement.find('.' + this.prefix));
+    
+    $(elements).each(function(i,v) {
+      var repeat = $(v);
+      repeat.css('background-color: rgba(255,0,255,0.4)');
+      $('body').append(repeat);
+    });
   }
 };
 
